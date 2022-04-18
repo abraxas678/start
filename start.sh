98ea952e67f9d97c7951905f1edb1d62cb851812
abraxas678 <abraxas678@gmail.com>
2022-04-18 10:50:48 +0200

    1

--- start.sh
+++ start.sh
@@ -10,7 +10,6 @@ rm rclonesetup.sh
 echo
 echo SHH
 echo
-<<<<<<< HEAD
 rclone copy gd:/sec/start/id_rsa.asc . -P
 gpg --decrypt id_rsa.asc > id_rsa
 rm *.asc
@@ -18,8 +17,6 @@ sudo mkdir $HOME/.ssh
 sudo chown abraxas678:100 $HOME -R
 sudo chmod 700 -R $HOME
 mv id_rsa $HOME/.ssh
-=======
->>>>>>> 808fd0759af0af39c8506c534c67abecb83c48fd
 eval `ssh-agent -s`
 sudo chmod 400 ~/.ssh/* -R
 ssh-add ~/.ssh/id_rsa
@@ -31,7 +28,6 @@ sudo chmod 600 ~/.ssh/id_rsa
 sudo chmod 644 ~/.ssh/id_rsa.pub
 echo
 git clone git@github.com:abraxas678/dotfiles.git
-
 
 
 
