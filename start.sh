598304962be9d8f7e035d8185f35cef5b4675617
abraxas678 <abraxas678@gmail.com>
2022-04-18 12:12:18 +0200

    1

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ts=$(date +"%s")
+sudo apt-get update && sudo apt-get upgrade -y
 cd $HOME
 git clone https://github.com/abraxas678/start.git
 cd start
@@ -34,7 +34,62 @@ sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -L git.io/antigen > antigen.zsh
 
+echo; echo; echo "INSTALL KEEPASSXC"
+echo
+#printf "${BLUE3}"
+sudo add-apt-repository ppa:phoerious/keepassxc -y
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+#printf "${BLUE1}"
+
+sudo apt-get install -y nano curl nfs-common xclip keepassxc ssh-askpass jq taskwarrior android-tools-adb conky-all
+
+echo
+echo FONTS
+echo
+myfonts="n"
+echo "WANT TO INSTALL FONTS? (y/n)"
+read -n 1 -t 20 myfonts
+if [[ $myfonts = "y" ]]; then
+  #https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+  curl -X POST -H "Content-Type: application/json" -d '{"myvar1":"foo","myvar2":"bar","myvar3":"foobar"}' "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=304c57b5ddbd4c10b03b76fa97d44559&deviceNames=razer,Chrome,ChromeRazer&text=play%20install%20this%20font&url=https%3A%2F%2Fgithub.com%2Fromkatv%2Fpowerlevel10k-media%2Fraw%2Fmaster%2FMesloLGS%2520NF%2520Regular.ttf&file=https%3A%2F%2Fgithub.com%2Fromkatv%2Fpowerlevel10k-media%2Fraw%2Fmaster%2FMesloLGS%2520NF%2520Regular.ttf&say=please%20install%20this%20font"
+  sudo apt update && sudo apt install -y zsh fonts-powerline xz-utils wget  
+  ###mlocate  -----> in tmu aufsetzen
+  ###### https://github.com/suin/git-remind
+  # sleep 1
+fi
+
+
+echo
+brewsetup="n"
+echo "START BREW SETUP?  (y/n)              --------------timeouut 20 n"
+echo
+read -t 20 -n 1 brewsetup
+echo
+if [[ $brewsetup != "n" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/abrax/.zprofile
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  sudo apt-get install build-essential -y
+  brew install gcc  
+fi
+
+brew install fd
+brew install fzf
+brew install thefuck
+$(brew --prefix)/opt/fzf/install
+brew install gcalcli
+echo
+echo AUTOREMOVE
+echo
+sudo apt autoremove -y
+
 cp $HOME/start/dotfiles/.zshrc $HOME/
 cp $HOME/start/dotfiles/.p10k.zsh $HOME/
 mv $HOME/start/dotfiles/bin/* $HOME/bin/
 
+
+echo
+echo EXEC ZSH
+echo
+exec zsh
