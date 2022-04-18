c4d77933eb74d2e26ea200f9d0c4d5a440c0eda7
abraxas678 <abraxas678@gmail.com>
2022-04-18 07:54:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sudo ssh keygen
 ts=$(date +"%s")
-curl -H "Authorization: token YourGeneratedToken" --data '{"title":"test-key","key":"$(cat id_rsa.pub)"}' https://api.github.com/user/keys
+cd $HOME
+git clone git@github.com:abraxas678/start.git
+cd start
+gpg --decrypt rclone_secure_setup2gd.sh.asc > rclonesetup.sh
+sudo chmod +x *.sh
+./rclonesetup.sh
+rm rclonesetup.sh
+echo
+echo SHH
+echo
 eval `ssh-agent -s`
 sudo chmod 400 ~/.ssh/* -R
 ssh-add ~/.ssh/id_rsa
@@ -13,4 +21,5 @@ sudo chmod 600 ~/.ssh/id_rsa
 sudo chmod 644 ~/.ssh/id_rsa.pub
 echo
 sudo git clone git@github.com:abraxas678/dotfiles.git
+
 
