3a528db97fcbe7af02b44c09e8a545ed2ca59fa7
abraxas678 <abraxas678@gmail.com>
2022-05-19 01:41:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 clear
+delstart="n"
+echo; echo "DELETE FOLDER START? (y/n)"; echo
+read delstart
+if [[ $deltstart = "y" ]]
+then
+  cd $HOME
+  rm -rf start
+fi
 echo
 echo SETUP GPG MANUALLY VIA OD VAULT
 echo
